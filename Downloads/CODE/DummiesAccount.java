9f447bf06fc342657aa58dac26780b507e21fe1d
thisisntsam <114687955+Weirdo10x@users.noreply.github.com>
2025-07-10 13:57:41 +0300

    Update on Code

--- Downloads/CODE/DummiesAccount.java
+++ Downloads/CODE/DummiesAccount.java
@@ -5,7 +5,7 @@ void: It does not return any value.
 String[] args: It takes an array of strings as arguments (for command-line input).*/
     double amountInAccount;
     amountInAccount=50.22;
-    amountInAccount= amountInAccount+ 1_000_000.00;
+    amountInAccount= amountInAccount+ 1000000.00;
     System.out.print("You have $");
     System.out.print(amountInAccount);
     System.out.println(" in your account.");
